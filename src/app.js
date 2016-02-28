dd027a38e39ea564e824be9c7793c838d639650f
andrew <stelma_28@bk.ru>
2016-02-28 20:50:54 +0300

    Methods for GrsuLoader.

--- src/app.js
+++ src/app.js
@@ -2,4 +2,18 @@ var grsuLoader = require("./GrsuLoader");
 
 var GrsuLoader = new grsuLoader.GrsuLoader();
 
-console.log(GrsuLoader.groupSchedule);
+/*GrsuLoader.LoadGroupSchedule(945, function (params) {
+    console.log(params);
+});*/
+
+/*GrsuLoader.LoadDepartments(function (params) {
+    console.log(params);
+});*/
+
+/*GrsuLoader.LoadFaculties(function (params) {
+    console.log(params);
+});*/
+
+/*GrsuLoader.LoadGroups(2,3,3,function (params) {
+    console.log(params);
+});*/
