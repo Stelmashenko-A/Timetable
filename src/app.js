2ae6ee457e1ac6e2d0e888c358a84fec7b8b239f
andrew <stelma_28@bk.ru>
2016-03-13 17:35:13 +0300

    mongoDb

--- src/app.js
+++ src/app.js
@@ -1,7 +1,20 @@
 var express = require('express');
 var router = require('./router');
+var mongoose = require('mongoose');
+// var tt = require('./timetable_miner');
+// var TimetableMiner = new tt.TimetableMiner();
+// TimetableMiner.loadAllTimetable(); 
+
+var router = express.Router();
 
 var app = express();
 app.use('/', router);
 
-app.listen(8888);
+app.listen(8888); 
+
+mongoose.connect('mongodb://localhost/test');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  console.log('rtyguhi');
+});
---
+++ src/index.js
@@ -0,0 +1,45 @@
+var http = require('http');
+var StringDecoder = require('string_decoder').StringDecoder;
+function GrsuLoader(params) {
+    this.host = 'api.grsu.by';
+    this.groupSchedule = '/1.x/app2/getGroupSchedule';// ?groupId=945
+    this.getDepartments = '/1.x/app2/getDepartments';
+    this.getFaculties = '/1.x/app2/getFaculties';
+    this.getGroups = '/1.x/app2/getGroups';// ?departmentId=2&facultyId=3&course=3
+};
+GrsuLoader.prototype.Load = function (path, callback) {
+    var body = [];
+    http.get({
+        hostname: this.host,
+        path: path,
+        agent: false
+    }, (res) => {
+        res.on('data', function (chunk) {
+            body.push(chunk);
+        }).on('end', function () {
+            callback(Buffer.concat(body).toString()); // body.toString() -> unexpected comma
+        }
+            );
+    });
+};
+GrsuLoader.prototype.loadGroupschedule = function (group, callback) {
+    var path = this.groupSchedule + '?groupId=' + group;
+    this.Load(path, callback);
+};
+
+GrsuLoader.prototype.loadDepartments = function (callback) {
+    var path = this.getDepartments;
+    this.Load(path, callback);
+};
+
+GrsuLoader.prototype.loadFaculties = function (callback) {
+    var path = this.getFaculties;
+    this.Load(path, callback);
+};
+
+GrsuLoader.prototype.loadGroups = function (departmentId, facultyId, course, callback) {
+    var path = this.getGroups + '?departmentId=' + departmentId + '&facultyId=' + facultyId + '&course=' + course;
+    this.Load(path, callback);
+};
+
+exports.GrsuLoader = GrsuLoader;
