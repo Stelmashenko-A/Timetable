a7e7fb91bfee793c6cfd86ccc16ccedefce344e3
andrew <stelma_28@bk.ru>
2016-04-02 15:56:40 +0300

    renamig

---
+++ src/grsu-srtucture.js
@@ -0,0 +1,8 @@
+function GrsuSrtucture() {
+    this.departments = [];
+    this.faculties = [];
+    this.courses = [];
+    this.groups = [];
+};
+
+exports.GrsuSrtucture = GrsuSrtucture;
--- src/timetable_miner.js
+++ src/timetable_miner.js
@@ -1,34 +1,30 @@
 var grsuLoader = require('./GrsuLoader');
 var async = require('async');
 var GrsuLoader = new grsuLoader.GrsuLoader();
-
+var GrsuSrtucture = require ('./grsu-srtucture').GrsuSrtucture;
 var http = require('http');
 
 function TimetableMiner(params) {
-    this.departments = [];
-    this.faculties = [];
-    this.courses = [];
-    this.groups = [];
 };
 
-function initUniversitySructure(timetable,callback) {
+function initUniversitySructure(grsuSrtucture,callback) {
     async.parallel([
     function (callback) {
         setTimeout(function () {
             GrsuLoader.loadDepartments(function (departments) {
-                timetable.departments = departments.items;
+                grsuSrtucture.departments = departments.items;
             callback();});
         }, 1000);
     },
     function (callback) {
         setTimeout(function () {
             GrsuLoader.loadFaculties(function (faculties) {
-                timetable.faculties = faculties.items;
+                grsuSrtucture.faculties = faculties.items;
             callback();});
         }, 1000);
     },
     function (callback) {
-        timetable.courses = [1, 2, 3, 4, 5];
+        grsuSrtucture.courses = [1, 2, 3, 4, 5];
         callback();
     }
 ], function () {
@@ -41,11 +37,11 @@ function RequestParams() {
     this.course;
 };
 
-function getRequestParams(timetable) {
+function getRequestParams(grsuSrtucture) {
     var paramsArray = [];
-    timetable.departments.forEach(function (department, i, departments) {
-        timetable.faculties.forEach(function (faculty, j, faculties) {
-            timetable.courses.forEach(function (course, k, courses) {
+    grsuSrtucture.departments.forEach(function (department, i, departments) {
+        grsuSrtucture.faculties.forEach(function (faculty, j, faculties) {
+            grsuSrtucture.courses.forEach(function (course, k, courses) {
                 var params = new RequestParams();
                 params.department = department;
                 params.faculty = faculty;
@@ -57,32 +53,34 @@ function getRequestParams(timetable) {
     return paramsArray;
 }
 
-function initGroups(timetable,callback) {
-    var requestParamsArray = getRequestParams(timetable);
-    timetable.groups = [];
+function initGroups(grsuSrtucture,callback) {
+    var requestParamsArray = getRequestParams(grsuSrtucture);
+    grsuSrtucture.groups = [];
     async.each(requestParamsArray, function (requestParams, callback) {
-        GrsuLoader.loadGroups(requestParams.department.id, requestParams.faculty.id, requestParams.course, function (groups) {
-                    groups.items.forEach(function (group, k, groups){
+        GrsuLoader.loadGroups(requestParams.department.id,
+         requestParams.faculty.id, requestParams.course, function (groups) {
+                    groups.items.forEach(function (group, k, groups) {
                         group.department = requestParams.department;
                         group.faculty = requestParams.faculty;
                         group.course = requestParams.course;
                     });
-                    timetable.groups = timetable.groups.concat(groups.items);                   
+                    grsuSrtucture.groups = grsuSrtucture.groups.concat(groups.items);
                     callback();
                 });
     }, callback);
 }
 
 TimetableMiner.prototype.loadAllTimetable = function (callback) {
+    var grsuSrtucture = new GrsuSrtucture();
     async.waterfall([
     function (callback) {
-        initUniversitySructure(TimetableMiner, callback);
+        initUniversitySructure(grsuSrtucture, callback);
     },
     function (callback) {
-        initGroups(TimetableMiner, callback);
+        initGroups(grsuSrtucture, callback);
     }
 ], function (params) {
-    console.log(TimetableMiner.groups);
+    console.log(grsuSrtucture.groups);
     callback();
 });
 };
